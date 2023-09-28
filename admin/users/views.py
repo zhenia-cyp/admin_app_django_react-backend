4764f45b594ff391b2a809f88c03cd2846540fd7
zhenia <zhenia@ubuntu20.04>
2023-09-28 13:43:15 +0300

    remade the AuthenticatedUser class

--- admin/users/views.py
+++ admin/users/views.py
@@ -53,9 +53,10 @@ class AuthenticatedUser(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
     def get(self, request):
-        serializer = UserSerializer(request.user)
+        data = UserSerializer(request.user).data
+        data['permissions']=[p['name'] for p in data['role']['permissions']]
         return Response({
-            'data': serializer.data
+            'data': data
         })
 
 
