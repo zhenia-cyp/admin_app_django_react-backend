c8813a5622dc67089ff32b1d729a228483f8c7a6
zhenia <zhenia@ubuntu20.04>
2023-10-04 09:56:39 +0300

    made the class ExportCsvAPIView for writing data to csv file

--- admin/orders/urls.py
+++ admin/orders/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import OrderGenericAPIView
+from .views import OrderGenericAPIView, ExportCsvAPIView
 
 urlpatterns = [
     path('orders/', OrderGenericAPIView.as_view()),
     path('order/<str:pk>/', OrderGenericAPIView.as_view()),
+    path('export/csv/', ExportCsvAPIView.as_view()),
 ]
 
--- admin/orders/views.py
+++ admin/orders/views.py
@@ -1,10 +1,13 @@
 from rest_framework import generics, mixins
 from users.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
-from orders.models import Order
+from orders.models import Order, OrderItem
 from orders.serializers import OrderSerializer
 from admin.pagination import CustomPagination
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.http import HttpResponse
+import csv
 
 
 
@@ -26,3 +29,27 @@ class OrderGenericAPIView(
             })
 
         return self.list(request)
+
+
+
+class ExportCsvAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=orders.csv'
+
+        orders = Order.objects.all()
+        writer = csv.writer(response)
+
+        writer.writerow(['ID', 'Name', 'Email', 'Product Title', 'Price', 'Quantity'])
+
+        for order in orders:
+            writer.writerow([order.id, order.name, order.email, '', '', ''])
+            orderItems = OrderItem.objects.all().filter(order_id=order.id)
+
+            for item in orderItems:
+                writer.writerow(['', '', '', item.product_title, item.price, item.quantity])
+
+        return response
